f0efb1c725b1bbc340f24a717c5672cd14184345
dennis <denismukuria@gmail.com>
2024-12-19 01:07:02 +0300

    apply find() function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(arr){
+    function checkWin(obf){
+        return obf.result === "W";
+    }
+    let win = arr.find(checkWin);
+    if (win==undefined){
+        return undefined;
+    }else {
+        return win.year;
+    }
+}
